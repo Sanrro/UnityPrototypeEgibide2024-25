8a4c28f9bb7a1af53eb9482f4b0111819ddd22ae
LaguardiaOraa <anderlaguardiaoraa@gmail.com>
2024-10-15 21:00:09 +0200

    Drag logic written
    Explained the logic of the program in the script

--- Assets/Code/DragNDrop.cs
+++ Assets/Code/DragNDrop.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DragNDrop : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //When click is pressed, search if any interactive card
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //If there is do
+
+    //Elevate y+10
+
+    //While click is pressed, maintain elevated + follow the mouse
+
+    //When dropped
+    
+    //if placeholder below --> set on top
+
+    //if not, return to original position
+    
 }
