94e0082f6993970122d7f45451cf7ee12d3a0ccc
LaguardiaOraa <anderlaguardiaoraa@gmail.com>
2024-10-15 21:12:45 +0200

    Script -- Edited
    Dragg part completed

--- Assets/Code/DragNDrop.cs
+++ Assets/Code/DragNDrop.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class DragNDrop : MonoBehaviour
 {
+    public GameObject draggedObject;
+    public Camera cam;
     //When click is pressed, search if any interactive card
 
     //If there is do
@@ -12,11 +14,14 @@ public class DragNDrop : MonoBehaviour
     //Elevate y+10
 
     //While click is pressed, maintain elevated + follow the mouse
-
+    private void Update()
+    {
+        draggedObject.transform.position = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5));
+    }
     //When dropped
-    
+
     //if placeholder below --> set on top
 
     //if not, return to original position
-    
+
 }
